d398748bf1d826ecc264fe451ec045d2be54c766
DELL <DELL@PRIYA>
2022-05-21 08:45:01 +0530

    hello

---
+++ git/src/com/A.java
@@ -0,0 +1,13 @@
+package com;
+
+public class A {
+	public static void main(String[] args) {
+		int[]a= {10,20,30,40,50,60};
+		for(int i=0;i<a.length;i++) {
+			if(i%2==1) {
+				System.out.println(a[i]);
+			}
+		}
+	}
+
+}
